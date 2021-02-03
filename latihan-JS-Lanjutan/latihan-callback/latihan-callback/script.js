28fe7279fd41ac3da1ad8d5e07b5b80b4faeb65f
Raggnel-Web <rizalfauzi774@gmail.com>
2021-02-03 23:41:06 +0700

    Menambahkan Latihan Callback

--- latihan-JS-Lanjutan/latihan-callback/latihan-callback/script.js
+++ latihan-JS-Lanjutan/latihan-callback/latihan-callback/script.js
@@ -1,31 +1,66 @@
-function getDataSiswa(url, success, error) {
-  let xml = new XMLHttpRequest();
-  xml.onreadystatechange = function () {
-    if (this.readyState === 4) {
-      if (this.status === 200) {
-        success(this.response)
-      } else if (this.status === 404) {
-        error();
-      }
+const input = document.querySelector(`.input-keyword`);
+const button = document.querySelector(`.search-button`);
+button.addEventListener('click', function () { 
+  $.ajax({
+    url: `http://www.omdbapi.com/?apikey=515b0ae4&s=${input.value}`,
+    success: (result) => {
+      const movies = result.Search
+      let cards = ``;
+      movies.forEach(movie => {
+        cards += /*html*/`
+        <div class="col-md-4 my-3">
+          <div class="card">
+            <img src="${movie.Poster}" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title">${movie.Title}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${movie.Year}</h6>
+              <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#Moviedetailmodal" data-imdb="${movie.imdbID}">See Details</a>
+            </div>
+          </div>
+        </div>
+      `
+      });
+      document.querySelector(`.movie-container`).innerHTML = cards;
+  
+      const films = document.querySelectorAll(`.modal-detail-button`);
+      films.forEach(movie => {
+        movie.addEventListener('click', function () {
+          $.ajax({
+            url: `http://www.omdbapi.com/?apikey=515b0ae4&i=${movie.dataset.imdb}`,
+            success: (result) => {
+              const movieDetail = /*html*/`
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-3">
+                    <img src="${result.Poster}" class="img-fluid">
+                  </div>
+                  <div class="col-md">
+                    <ul class="list-group">
+                      <li class="list-group-item"><h4>${result.Title} (${result.Year})</h4></li>
+                      <li class="list-group-item"><strong>Director : </strong>${result.Director}</li>
+                      <li class="list-group-item"><strong>Actors : </strong>${result.Actors}</li>
+                      <li class="list-group-item"><strong>Writer : </strong>${result.Writer}</li>
+                      <li class="list-group-item"><strong>Plot : </strong><br>${result.Plot}</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              `;
+              document.querySelector(`.modal-body`).innerHTML = movieDetail;
+            },
+            error(e) {
+              console.log(e.responseText);
+            }
+          })
+        });
+      });
+    },
+    error: (e) => {
+      console.log(e.responseText);
     }
-  }
-  xml.open(`get`, url);
-  xml.send();
-};
-
-console.log(`mulai`);
-getDataSiswa(`data/siswa.json`, (result) => {
-  const siswa = JSON.parse(result);
-  console.table(siswa);
-  siswa.forEach(swa => {
-    console.log(swa.nama);
-    console.log(swa.email);
   });
-  // menampilkan dalam bentuk objek
-}, () => {
-    
-});
-console.log(`selesai`);
-// selesai akan jalan duluan karena asyn jalan paling terakhir
-// asyin akan masuk ke dalam web API
-// dan akan menunggu semua perintah selesai dikerjakan
+ })
+
+
+
+
